d2bdd44bca18a145bc5680ded3b57fb820a974ea
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-11-07 16:55:48 +0100

    Added problem description in the main method.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        /**
+         * Given an array of integers 'nums'and an integer 'target', retun indices of
+         * the two numbers such that they add up to the target.
+         * 
+         * You can assume that each input would have exactly one solution, and you may
+         * not use the same elemnt twice.
+         * 
+         * You can return the anser in any order.
+         * 
+         * nums:[15,7,2,11]--> target:9
+         * ans: [1,2] or[2,1]
+         */
         System.out.println("Hello, World!");
     }
 }
